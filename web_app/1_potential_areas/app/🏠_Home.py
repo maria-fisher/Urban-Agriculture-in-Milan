cee299d7ee5ec8e78e406ebf962b689f94aa73d2
Pujan Thapa <iampujan@outlook.com>
2024-07-02 16:50:26 +0200

    renamed filenames and changed homepage, removed top links for pages

--- web_app/1_potential_areas/app/🏠_Home.py
+++ web_app/1_potential_areas/app/🏠_Home.py
@@ -1,17 +1,6 @@
 import streamlit as st
 
-def intorduction_page():
-
-    col1, col2, col3, col4 = st.columns([1,1,3,1], gap='medium')
-    with col1:
-        st.page_link(r"pages/1_home.py", label="Home", icon="🏠")
-    with col2:
-        st.page_link(r"pages/2_eda.py", label="EDA", icon="📶")
-    with col3:
-        st.page_link(r"pages/3_prediction.py", label="Find Suitable Area for Uraban Farming", icon="🤖")
-    with col4:
-        st.page_link(r"pages/4_contact.py", label="Contact Us", icon="📧")
-
+def introduction_page():
     # st.set_page_config(page_title="Home", page_icon=':house:')
     st.title("Identify Areas for Urban Agriculture in Milan")
     st.header("Introduction")
@@ -21,5 +10,4 @@ def intorduction_page():
     in improving food security, reducing carbon footprints, and utilizing vacant land efficiently.
     """)
 
-if __name__ == "__main__":
-    intorduction_page()
+introduction_page()
