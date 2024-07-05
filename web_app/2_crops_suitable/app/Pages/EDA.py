336bb345a4b23212fd92d2f48a529d6f76465243
swathy06 <sujswa@gmail.com>
2024-07-05 19:23:00 +0200

    changes in EDA

--- web_app/2_crops_suitable/app/Pages/EDA.py
+++ web_app/2_crops_suitable/app/Pages/EDA.py
@@ -1,26 +1,65 @@
-import streamlit as st
-import streamlit.components.v1 as components
-from st_pages import add_page_title
-from utils import utils
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 22:24:06 2024
 
-def app():
-    # Title of the EDA page
+@author: rampr
+"""
+
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+
+def load_data():
+    return pd.read_csv(r'C:\Users\rampr\Documents\Omdena-Urban-Agriculture-in-Milan\data\2_crops_suitable\processed\preprocessed_700.csv')
+
+# Define a function to display EDA graphs
+def display_eda(data):
     st.title("Exploratory Data Analysis")
 
-    # Looker Studio report URL
-    report_url = "https://lookerstudio.google.com/embed/reporting/308794df-40c2-4857-a909-ab689dbcfb3e/page/p_iluq9g1zhd"
+    
 
-    # Create the iframe HTML
-    iframe_code = f"""
-    <iframe width="100%" height="1000" src="{report_url}" frameborder="0" style="border:0" allowfullscreen></iframe>
-    """
+    # Correlation Heatmap
+    st.subheader("Correlation Heatmap")
+    plt.figure(figsize=(10, 4))
+    heatmap_fig = sns.heatmap(data.corr(), annot=True, cmap='coolwarm')
+    st.pyplot(heatmap_fig.figure)
 
-    # Display the iframe using Streamlit's components.html function
-    components.html(iframe_code, height=1000)
+    # Histogram
+    st.subheader("Histogram")
+    column = st.selectbox("Select a column for histogram", data.columns)
+    hist_fig = px.histogram(data, x=column)
+    st.plotly_chart(hist_fig)
 
-utils.set_custom_bg()
-utils.custom_navbar()
-# Sidebar image
-st.sidebar.image("./media/omdena_logo_navigation.png", use_column_width='always')
-app()
-add_page_title(layout="wide")
+    # Boxplot
+    st.subheader("Boxplot")
+    column = st.selectbox("Select a column for boxplot", data.columns)
+    box_fig = px.box(data, y=column)
+    st.plotly_chart(box_fig)
+
+    # Violin Plot
+    st.subheader("Violin Plot")
+    column = st.selectbox("Select a column for violin plot", data.columns)
+    violin_fig = px.violin(data, y=column)
+    st.plotly_chart(violin_fig)
+
+    # Scatter Plot
+    st.subheader("Scatter Plot")
+    x_column = st.selectbox("Select x column for scatter plot", data.columns)
+    y_column = st.selectbox("Select y column for scatter plot", data.columns)
+    scatter_fig = px.scatter(data, x=x_column, y=y_column)
+    st.plotly_chart(scatter_fig)
+
+    # Line Plot
+    st.subheader("Line Plot")
+    line_x_column = st.selectbox("Select x column for line plot", data.columns)
+    line_y_column = st.selectbox("Select y column for line plot", data.columns)
+    line_fig = px.line(data, x=line_x_column, y=line_y_column)
+    st.plotly_chart(line_fig)
+
+# Load data
+data = load_data()
+
+# Display EDA graphs
+display_eda(data)
